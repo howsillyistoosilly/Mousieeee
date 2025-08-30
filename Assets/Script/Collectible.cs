020ef8eb318f970f7ce421805e05eb7418ef059a
keshitrivedi <keshitrivedi@gmail.com>
2025-08-31 02:14:58 +0530

    Add Choesse

--- Assets/Script/Collectible.cs
+++ Assets/Script/Collectible.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public GameObject mousie;
+    public bool isCollected = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.SetActive(false);
+        isCollected = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 0f, 50f * Time.deltaTime);
     }
 }
